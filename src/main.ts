e4491bf75f19cac69d3d40a9ddbc353a53fcb657
thangnv6 <thangnv@foobla.com>
2021-04-28 00:55:07 +0700

    config to build

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,5 @@ import App from './App.vue'
 import './assets/css/css.css'
 import './assets/css/jquery-ui-1.10.3.custom.min.css'
 import './assets/css/main.css'
-
+import './assets/scripts/main.js'
 createApp(App).mount('#app')
